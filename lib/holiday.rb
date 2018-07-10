221b2d3f4dcfdff09f021d4e6913eec1c99268f8
Pablo Argueta <pabloargueta27@gmail.com>
2018-07-10 05:00:57 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,7 +82,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each { |season, holiday|
     holiday.collect { |holiday, value|
       if value.include?("BBQ")
-        value
+        holiday
       end
     }
   }
